03eca650aad953518d9c899d19b3350183b631ca
Debargha Nandi <debarghanandi.india@gmail.com>
2025-01-23 23:40:59 +0530

    Update

---
+++ 7. onClick Function/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <button onClick={()=>(alert("Hi Broo"))}>Show Allart</button>
+    </div>
+  )
+}
+
+export default App
