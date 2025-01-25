af94d3fecaa11c7b169c2c5985a45f2fe12d36c0
Debargha Nandi <debarghanandi.india@gmail.com>
2025-01-26 01:58:36 +0530

    Update

---
+++ 13. Light and Dark Mode/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+        <h1>Debargha Nandi</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, fugit quam. Quaerat distinctio autem explicabo porro, odit fugiat molestiae ea iusto quae ab aspernatur sapiente? A ea perferendis beatae quod sequi! Sit provident expedita, veniam optio nam delectus corrupti accusamus quis aut, totam animi dolorem! Culpa, dolore aperiam! Facilis, ut eos! Nobis quo accusamus blanditiis eveniet? Beatae blanditiis qui sequi numquam quia et minus veritatis sed perferendis, accusantium fuga tenetur, nesciunt consequatur eveniet repellendus dignissimos perspiciatis quisquam? Animi illo tenetur incidunt rem, sed illum perferendis! Perferendis cupiditate dolor, delectus optio nostrum voluptatem omnis facere eveniet eius odit fuga aliquam est. Ipsum ut laborum quia molestiae repudiandae mollitia dicta dolorem veniam sed numquam iure eligendi ea doloribus id sunt rem consequatur ex cumque sint, cum quaerat molestias amet recusandae? Error, expedita et sunt temporibus ducimus nostrum itaque obcaecati, maxime voluptatum magni sit culpa aliquam repellendus vel nesciunt. Facilis placeat labore iusto ipsam officia accusamus. Magni nulla vel minima distinctio accusamus nemo eligendi labore fugiat repellat, possimus cum exercitationem quos quibusdam omnis fuga molestias placeat debitis vitae. Magni officiis adipisci nam quod commodi ipsum ad optio expedita deleniti sit ratione, praesentium quae consequuntur fugit vitae sunt reiciendis quibusdam similique facilis! Adipisci, officia?</p>
+    </div>
+  )
+}
+
+export default Body
